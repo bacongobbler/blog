0e1a2bd3dad814bfe25f560b572afccc9e7a7e6c
Matthew Fisher <matt.fisher@microsoft.com>
2018-07-27 12:10:34 -0700

    more CDN removal

--- release.sh
+++ release.sh
@@ -10,8 +10,6 @@ usage() {
 	- to be provided with the name of the resource group these assets live in, in environment variable AZURE_RG_NAME
 	- to be provided with the name of an Azure Blob Storage Container, in environment variable AZURE_CONTAINER
 	- to be provided with Azure credentials for the container, in environment variables AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_KEY
-	- to be provided with the name of the Azure CDN, in environment variable AZURE_CDN_NAME
-	- to be provided with the name of the endpoint in the Azure CDN, in environment variable AZURE_CDN_ENDPOINT_NAME
 	EOF
 	exit 1
 }
@@ -28,7 +26,6 @@ fi
 [ "$AZURE_CONTAINER" ] || usage
 [ "$AZURE_STORAGE_ACCOUNT" ] || usage
 [ "$AZURE_STORAGE_KEY" ] || usage
-[ "$AZURE_CDN_NAME" ] || usage
 
 # upload the files to azure
 az storage blob upload-batch --source public/ --destination "${AZURE_CONTAINER}"
