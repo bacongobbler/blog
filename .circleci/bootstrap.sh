4852e88b96062bc06cadf1a0ae7865bab1d4f708
Matthew Fisher <matt.fisher@microsoft.com>
2020-04-22 14:39:26 -0700

    Replace CircleCI with Github Actions
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- .circleci/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-set -euo pipefail
-
-apt update -y && apt install -yq curl git sudo
-
-curl -fsSL https://raw.githubusercontent.com/fishworks/gofish/master/scripts/install.sh | bash
-gofish init
-gofish install hugo
